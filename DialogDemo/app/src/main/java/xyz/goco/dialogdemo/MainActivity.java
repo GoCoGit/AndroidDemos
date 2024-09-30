89180b9b7b6389531b1432a0938e5b111473fc9b
guochen <guochen8@xiaomi.com>
2024-09-30 15:19:00 +0800

    dialogdemo

---
+++ DialogDemo/app/src/main/java/xyz/goco/dialogdemo/MainActivity.java
@@ -0,0 +1,58 @@
+package xyz.goco.dialogdemo;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private View inflate;
+    private TextView choosePhoto;
+    private TextView takePhoto;
+    private Dialog dialog;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+    public void show(View view){
+        dialog = new Dialog(this,R.style.ActionSheetDialogStyle);
+        //填充对话框的布局
+        inflate = LayoutInflater.from(this).inflate(R.layout.dialog, null);
+        //初始化控件
+        choosePhoto = (TextView) inflate.findViewById(R.id.choosePhoto);
+        takePhoto = (TextView) inflate.findViewById(R.id.takePhoto);
+        choosePhoto.setOnClickListener(this);
+        takePhoto.setOnClickListener(this);
+        //将布局设置给Dialog
+        dialog.setContentView(inflate);
+        //获取当前Activity所在的窗体
+        Window dialogWindow = dialog.getWindow();
+        //设置Dialog从窗体底部弹出
+        dialogWindow.setGravity(Gravity.BOTTOM);
+        //获得窗体的属性
+        WindowManager.LayoutParams lp = dialogWindow.getAttributes();
+        lp.y = 20;//设置Dialog距离底部的距离
+//       将属性设置给窗体
+        dialogWindow.setAttributes(lp);
+        dialog.show();//显示对话框
+    }
+
+    @Override
+    public void onClick(View view) {
+        dialog.dismiss();
+    }
+}
